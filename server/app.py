8b3490f27f9653d8d9005506ca1b593c6ed07707
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-09-25 18:48:54 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,88 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.all():
+            message_dict = {
+                "id": message.id,
+                "body": message.body,
+                "username": message.username,
+                "created_at": message.created_at,
+                "updated_at": message.updated_at,
+            }
+            messages.append(message_dict)
+
+        response = make_response(
+            jsonify(messages),
+            200
+        )
+        
+        return response
+
+    elif request.method == 'POST':
+
+        post_body = request.get_json()
+
+        new_message = Message(
+            username=post_body["username"],
+            body=post_body["body"],
+        )
+
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."    
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+
+    elif request.method == 'PATCH':
+
+        patch_dict = request.get_json()
+
+        for attr in patch_dict:
+            setattr(message, attr, patch_dict[attr])
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            200
+        )
+
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
